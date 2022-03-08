d75f3a4f595982ec7f35c95fc453dc3df1ee41d2
enti <enti@enti.enti>
2022-03-08 14:21:22 +0100

    Funcionando pero sin reloj

--- ejemplo_react/src/Piesito.js
+++ ejemplo_react/src/Piesito.js
@@ -1,10 +1,20 @@
 import './Piesito.css';
-function Piesito ()
+import React from 'react';
+
+class Piesito extends React.Component
 {
-	return (
-		<footer>
-			<p>La mama de Piesito (la que se muere)</p>
-		</footer>
-	);
+	constructor() {
+		super();
+		this.state ={
+			
+		};
+	}
+	render() {
+		return (
+			<footer>
+				<p>La mama de Piesito (la que se muere)</p>
+			</footer>
+		);
+	}
 }
 export default Piesito;
